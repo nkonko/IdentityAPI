6be2e9386e560a7161c527b0fb8e29251c20c616
nkonko <nicoazzara@hotmail.com>
2025-09-10 13:33:19 +0200

    added migration

--- identityAPI.Infrastructure/Migrations/20250905191818_AddProfileFieldsToUser.Designer.cs
+++ identityAPI.Infrastructure/Migrations/20250905191818_AddProfileFieldsToUser.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using identityAPI.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using identityAPI.Infrastructure.Persistence;
 namespace identityAPI.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250905191818_AddProfileFieldsToUser")]
+    partial class AddProfileFieldsToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ identityAPI.Infrastructure/Migrations/20250905191818_AddProfileFieldsToUser.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace identityAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProfileFieldsToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Bio",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLogin",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Position",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePictureUrl",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bio",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastLogin",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ProfilePictureUrl",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "AspNetUsers");
+        }
+    }
+}
